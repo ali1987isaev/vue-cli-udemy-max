350260f61c9459470b9bef2d4272f366fd0e9e43
Ali <ali@tomorrowagency.com>
2023-10-02 10:54:16 +0100

    first-component

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,66 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorit ? '(Favorit)' : '' }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavorite">Toggle Favorit</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone: </strong> {{ phone }}
+      </li>
+      <li>
+        <strong>Email: </strong> {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      isFavorit: {
+        type: Boolean,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      phone: {
+        type: String,
+        required: true
+      },
+    },
+    emits: ['toggle-favorite'],
+    // emits: {
+    //   'toggle-favorite': function(id) {
+    //     if (id) {
+    //       return true;
+    //     } else {
+    //       console.warn('ID is missing!');
+    //       return false;
+    //     }
+    //   },
+    // },
+    data() {
+      return {
+        detailsAreVisible: false,
+      }
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+      toggleFavorite() {
+        this.$emit('toggle-favorite', this.id);
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
+import App from './App.vue'
 
-createApp({}).mount('');
+import FriendContact from './components/FriendContact.vue'
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
