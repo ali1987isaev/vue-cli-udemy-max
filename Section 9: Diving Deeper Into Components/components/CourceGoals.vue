6f0fb9708a240688988f572c2b9e7cc62919401b
Ali <ali@tomorrowagency.com>
2023-10-05 22:37:15 +0100

    diving-deeper-into-components

---
+++ Section 9: Diving Deeper Into Components/components/CourceGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" anotherProp="test"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        goals: ['Complete the course.', 'Learn the Vue.js.']
+      }
+    }
+  }
+</script>
